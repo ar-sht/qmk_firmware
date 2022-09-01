9138eb9c91434de42f74d32e3313d0a0b863a6aa
Ari Shtein <ashtein120@gmail.com>
2022-09-01 13:52:35 -0400

    Now has QWERTY layer and RGBLED layer, no more wpm animation bc of memory stuff

--- keyboards/crkbd/keymaps/ar-sht/keymap.c
+++ keyboards/crkbd/keymaps/ar-sht/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BSPC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I, KC_O, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  XXXXXXX,
+      TO(5),    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  TO(6),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  KC_LSFT,     KC_SPC,   MO(2), KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -80,122 +80,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, XXXXXXX,  _______,     XXXXXXX, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
+  ),
+
+  [4] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        XXXXXXX, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX,                  UC(0x260), KC_AMPR, KC_GRV, KC_TILD, UC(0x00B1), XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                     UC(0x20A9), KC_DLR, KC_PERC, KC_CIRC, KC_UNDS, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN3, KC_BTN5, XXXXXXX,                  UC(0x00B0), KC_EXLM, KC_AT, KC_HASH, UC(0x00F7), XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, XXXXXXX,  _______,     XXXXXXX, _______, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [5] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H,    KC_J,    KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      TO(0), KC_Z,  KC_X,  KC_C, KC_V, KC_B,                                     KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TO(6),
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LCTRL,   MO(1),  KC_LSFT,     KC_SPC,   MO(2), KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [6] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXX,    RGB_M_P,    RGB_M_B,    RGB_M_R,    RGB_M_SW,    RGB_M_SN,                         RGB_M_K,    RGB_M_X,    RGB_M_G,    RGB_M_T,   RGB_M_TW,  XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,    RGB_VAI,    RGB_SAI, RGB_HUI, RGB_MOD, RGB_TOG,                               RGB_TOG , RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                         XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,  TO(0),
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          XXXXXXX,   XXXXXXX,  XXXXXXX,     XXXXXXX,   XXXXXXX, XXXXXXX
+                                      //`--------------------------'  `--------------------------'
   )
 };
-
-#define _MA 0
-#define _FN 1
-int timer = 0;
-char wpm_text[5];
-int x = 31;
-int currwpm = 0;
-int vert_count = 0; 
-
-//=============  USER CONFIG PARAMS  ===============
-float max_wpm = 110.0f; //WPM value at the top of the graph window
-int graph_refresh_interval = 80; //in milliseconds
-int graph_area_fill_interval = 3; //determines how dense the horizontal lines under the graph line are; lower = more dense
-int vert_interval = 3; //determines frequency of vertical lines under the graph line
-bool vert_line = false; //determines whether to draw vertical lines
-int graph_line_thickness = 3; //determines thickness of graph line in pixels
-//=============  END USER PARAMS  ===============
-
-enum custom_keycodes {
-  KC_CUST = SAFE_RANGE,
-};
-
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
-}
-
-#define L_BASE 0
-#define L_EXTEND 2
-#define L_SYMBOLS_1 4
-#define L_FUNCTION 8
-#define L_SYMBOlS_2 10
-
-void oled_wpm_animation(void) {
-	
-	//get current WPM value
-	currwpm = get_current_wpm();
-	
-	//check if it's been long enough before refreshing graph
-	if(timer_elapsed(timer) > graph_refresh_interval){
-	
-		// main calculation to plot graph line
-		x = 32 - ((currwpm / max_wpm) * 32);
-		
-		//first draw actual value line
-		for(int i = 0; i <= graph_line_thickness - 1; i++){
-			oled_write_pixel(1, x + i, true);
-		}
-		
-		
-		//then fill in area below the value line
-		if(vert_line){
-			if(vert_count == vert_interval){
-				vert_count = 0;
-				while(x <= 32){
-					oled_write_pixel(1, x, true);
-					x++;
-				}
-			} else {
-				for(int i = 32; i > x; i--){
-					if(i % graph_area_fill_interval == 0){
-						oled_write_pixel(1, i, true);
-					}
-				}
-				vert_count++;
-			}
-		} else {
-			for(int i = 32; i > x; i--){
-				if(i % graph_area_fill_interval == 0){
-					oled_write_pixel(1, i, true);
-				}
-			}
-		}
-		
-		//then move the entire graph one pixel to the right
-		oled_pan(false); 
-		
-		//refresh the timer for the next iteration
-		timer = timer_read();
-		
-	}
-	
-	//format current WPM value into a printable string
-	sprintf(wpm_text,"%i", currwpm);
-	
-	//formatting for triple digit WPM vs double digits, then print WPM readout
-	if(currwpm >= 100){
-		oled_set_cursor(14, 3);
-		oled_write("WPM: ", false);
-		oled_set_cursor(18, 3);
-		oled_write(wpm_text, false);
-	} else if (currwpm >= 10){
-		oled_set_cursor(15, 3);
-		oled_write("WPM: ", false);
-		oled_set_cursor(19, 3);
-		oled_write(wpm_text, false);
-	} else if (currwpm > 0) {
-		oled_set_cursor(16, 3);
-		oled_write("WPM: ", false);
-		oled_set_cursor(20, 3);
-		oled_write(wpm_text, false);		
-	}
-	
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_wpm_animation();
-    } else {
-        oled_wpm_animation();
-    }
-    return false;
-}
-#endif // OLED_ENABLE
